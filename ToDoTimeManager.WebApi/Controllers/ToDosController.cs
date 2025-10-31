7591dbb7274ef596f918d88da93b1e42f8ae2a32
Ffaize <dmitro.danko@gmail.com>
2025-10-31 12:25:12 +0200

    added controllers empty

---
+++ ToDoTimeManager.WebApi/Controllers/ToDosController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoTimeManager.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDosController : ControllerBase
+    {
+    }
+}
---
+++ ToDoTimeManager.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoTimeManager.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+    }
+}
