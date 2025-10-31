0e02f8dd439700834c34fe0906f626f8c118a57d
Ffaize <dmitro.danko@gmail.com>
2025-10-31 14:45:54 +0200

    added db Acces Services

---
+++ ToDoTimeManager.WebApi/Services/DbAccessServices/StoredProceduresNames.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoTimeManager.WebApi.Services.DbAccessServices
+{
+    public static class StoredProceduresNames
+    {
+        #region Users
+
+        #endregion
+
+        #region ToDos
+
+        #endregion
+
+        #region TimeLogs
+
+        #endregion
+
+    }
+}
