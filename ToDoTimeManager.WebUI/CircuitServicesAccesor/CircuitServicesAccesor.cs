ce1de43d885d0ca0fd5d04e3ab738786ce4f363e
Ffaize <dmitro.danko@gmail.com>
2025-10-31 12:49:12 +0200

    added circuitservice Accesor

---
+++ ToDoTimeManager.WebUI/CircuitServicesAccesor/CircuitServicesAccesor.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoUI.CircuitServicesAccesors
+{
+    public class CircuitServicesAccesor
+    {
+        static readonly AsyncLocal<IServiceProvider> _blazoredService = new();
+
+        public IServiceProvider? Service
+        {
+            get => _blazoredService.Value;
+            set => _blazoredService.Value = value;
+        }
+    }
+}
