99d7331b71315789c3d069794646fb906ba4d738
Ffaize <dmitro.danko@gmail.com>
2025-11-08 01:47:49 +0200

    Add localization support and UI enhancements
    Introduced localization with support for `en-US` and `uk-UA` cultures.
    Configured `RequestLocalizationOptions` and added a `CultureController`
    for culture switching via cookies. Implemented a `CultureSelector`
    component for language selection in the UI.
    
    Updated `Program.cs` to register localization services and middleware.
    Added resource files (`Resource.resx`, `Resource.en-US.resx`,
    `Resource.uk-UA.resx`) for localized strings.
    
    Enhanced toast notifications with improved spacing and responsive font
    sizes. Updated `site.min.css` and `toasts.css` for better UI consistency.
    
    Refactored and added new namespaces, services, and components to support
    localization and improve user experience.

--- ToDoTimeManager.WebUI/Program.cs
+++ ToDoTimeManager.WebUI/Program.cs
@@ -1,5 +1,7 @@
+using ToDoTimeManager.WebUI.Services.CircuitServicesAccesor;
 using ToDoTimeManager.WebUI.Services.Implementations;
 using ToDoTimeManager.WebUI.Services.Interfaces;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace ToDoTimeManager.WebUI;
 
@@ -9,14 +11,31 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
         // Add services to the container.
         builder.Services.AddRazorPages();
+        builder.Services.AddCircuitServicesAccesor();
         builder.Services.AddServerSideBlazor();
+        builder.Services.AddLocalization();
+        builder.Services.AddServerSideBlazor()
+            .AddCircuitOptions(options =>
+            {
+                options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromDays(1);
+            });
 
         builder.Services.AddScoped<IToastMessagesService, ToastMessagesService>();
 
 
         var app = builder.Build();
+
+        string[] supportedCultures = ["en-US", "uk-UA"];
+        var localizationOptions = new RequestLocalizationOptions()
+            .SetDefaultCulture(supportedCultures[1])
+            .AddSupportedCultures(supportedCultures)
+            .AddSupportedUICultures(supportedCultures);
+
+        app.UseRequestLocalization(localizationOptions);
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
@@ -27,12 +46,10 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
         app.UseStaticFiles();
-
         app.UseRouting();
-
         app.MapBlazorHub();
+        app.MapControllers();
         app.MapFallbackToPage("/_Host");
 
         app.Run();
