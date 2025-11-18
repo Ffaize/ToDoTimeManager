c5f220ec38b5291d324147ab781a5c08be3d8cde
Ffaize <dmitro.danko@gmail.com>
2025-11-18 14:41:41 +0200

    fixed toast logic and css

--- ToDoTimeManager.WebUI/Services/Implementations/ToastsService.cs
+++ ToDoTimeManager.WebUI/Services/Implementations/ToastsService.cs
@@ -1,3 +1,6 @@
+using System.Timers;
+using Timer = System.Timers.Timer;
+
 namespace ToDoTimeManager.WebUI.Services.Implementations;
 
 public class ToastsService
@@ -6,20 +9,51 @@ public class ToastsService
     public Action? OnChange { get; set; }
     public void ShowToast(string message, bool isError)
     {
-        Messages.Add(new ToastModel(message, isError));
+        Messages.Add(new ToastModel(message, isError, this));
         OnChange?.Invoke();
     }
 }
 
-public class ToastModel
+
+public class ToastModel : IDisposable
 {
     public Guid Id { get; set; }
     public string Message { get; set; }
     public bool IsError { get; set; }
-    public ToastModel(string message, bool isError)
+    public Timer Timer { get; set; }
+    private ToastsService ToastsService { get; set; }
+    public ToastModel(string message, bool isError, ToastsService toastsService)
     {
         Id = Guid.NewGuid();
         Message = message;
         IsError = isError;
+        ToastsService = toastsService;
+        CreateTimer();
+        Timer?.Start();
+    }
+    private void CreateTimer()
+    {
+        Timer = new Timer();
+        Timer.AutoReset = false;
+        Timer.Enabled = true;
+        Timer.Interval = 4000;
+        Timer.Elapsed += TimerOnElapsed;
+    }
+
+    private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+    {
+        RemoveToast();
+    }
+
+    public void RemoveToast()
+    {
+        ToastsService.Messages.Remove(this);
+        ToastsService.OnChange?.Invoke();
+        Dispose();
+    }
+
+    public void Dispose()
+    {
+        Timer.Elapsed -= TimerOnElapsed;
     }
 }
