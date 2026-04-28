dbd84b48db0f539aa9cfb8f830183ec9f9421a0e
Ffaize <dmitro.danko@gmail.com>
2026-04-28 13:41:06 +0300

    added test modal

--- ToDoTimeManager.WebUI/Pages/AuthPage.razor.cs
+++ ToDoTimeManager.WebUI/Pages/AuthPage.razor.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Localization;
+using ToDoTimeManager.WebUI.Components.Modals;
 using ToDoTimeManager.WebUI.Models.Enums;
 using ToDoTimeManager.WebUI.Resources;
 using ToDoTimeManager.WebUI.Services.HttpServices;
 using ToDoTimeManager.WebUI.Services.Interfaces;
+using ToDoTimeManager.WebUI.Services.Modal;
 
 namespace ToDoTimeManager.WebUI.Pages;
 
@@ -16,6 +18,7 @@ public partial class AuthPage
     [Inject] private IToastsService ToastsService { get; set; } = null!;
     [Inject] private NavigationManager NavigationManager { get; set; } = null!;
     [Inject] private ILogger<AuthPage> Logger { get; set; } = null!;
+    [Inject] private IModalService ModalService { get; set; } = null!;
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
@@ -27,6 +30,12 @@ public partial class AuthPage
             await ToastsService.ShowToast(Localizer["AuthPageLoaded"], ToastType.Success);
             await Task.Delay(1000);
             await ToastsService.ShowToast(Localizer["AuthPageLoaded"], ToastType.Warning);
+            await Task.Delay(5000);
+            var parameters = new ModalParameters();
+            parameters.Add("MessageDetails", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            var modal = ModalService.Show<ConfirmModal>("Заголовок", parameters);
+            var confirmed = await modal.Result;
 
         }
         await base.OnAfterRenderAsync(firstRender);
