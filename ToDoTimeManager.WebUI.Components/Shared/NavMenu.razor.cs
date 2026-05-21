2fe5d6b54d54d9ec2c295537262e70d223fd4629
Ffaize <dmitro.danko@gmail.com>
2026-05-21 18:22:22 +0300

    Refactor sidebar and NavMenu UI/logic
    Redesigned sidebar as a flex column and simplified NavMenu by removing collapse logic and toggler. Added brand row, search input, and profile dropdown to NavMenu. Moved NavMenu logic to code-behind with new state management. Updated CSS for sidebar, search, and profile dropdown with backdrop blur. Scene component now supports optional shadow. Cleaned up unused code and parameters.

---
+++ ToDoTimeManager.WebUI.Components/Shared/NavMenu.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ToDoTimeManager.WebUI.Components.Shared;
+
+public partial class NavMenu
+{
+    private bool IsOpenProfileDropdown { get; set; }
+    [Inject] private IStringLocalizer<Resource> Localizer { get; set; } = null!;
+    public string SearchToDoValue { get; set; } = string.Empty;
+
+    private Task OnEnterPressed()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SwitchProfileDropdownState()
+    {
+        IsOpenProfileDropdown = !IsOpenProfileDropdown;
+        InvokeAsync(StateHasChanged);
+    }
+
+    private Task OnProfileClicked()
+    {
+        throw new NotImplementedException();
+    }
+}
